f9edb00e54269e21f71c921a494bda55e62a4bfb
Abhishek Sharma <abhishekshaxma8356@gmail.com>
2022-10-29 02:25:41 +0530

    v1

---
+++ App.js
@@ -0,0 +1,4 @@
+// localStorage.setItem("name", "abhishek");
+// console.log(localStorage.getItem("roll"));
+sessionStorage.setItem("name", "abhishek");
+cobsole.log(sessionStorage.getItem("name"));
